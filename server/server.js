84680d29e956c15d6efde953f72173188c6b25d0
Michael Mote <michael.mote1993@gmail.com>
2023-12-21 14:06:32 -0600

    error handling added

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
-// dotenv .config() reads the .env file and saves the environment variables
 require('dotenv').config();
 
 // Import dependencies
-const express = require('express');
+const express = require('express'); 
 const app = express();
 const cors = require('cors'); 
-const knex = require('knex'); // This is a query builder
-const knexConfig = require('./knexfile.js'); // This is the knex configuration file for the database
+const knex = require('knex');
+const knexConfig = require('./knexfile.js'); 
 const db = knex(knexConfig); // This is the database connection
-// const todoRoutes = require('./routes/todoRoutes.js');
+const todoRoutes = require('./routes/todoRoutes.js');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-// app.use('/todos', todoRoutes);
+app.use('/api', todoRoutes);
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: `Internal Server Error` });
+})
 
 const PORT = process.env.PORT || 6969;
 app.listen(PORT, () => {
