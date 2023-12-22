3a131e1bca8df3b1c5ccefb676670fa2158da984
Michael Mote <michael.mote1993@gmail.com>
2023-12-21 23:27:36 -0600

    app.js still in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,112 @@
-import './App.css';
+import "./App.css";
 
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import NavBar from './components/NavBar';
-import LoginForm from './components/LoginForm';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+import NavBar from "./components/NavBar";
+import LoginForm from "./components/LoginForm";
+import TodoList from "./components/TodoList";
+// import TodoEditForm from './components/TodoEditForm';
+import TodoCreate from "./components/TodoCreate";
+import TodoSort from "./components/TodoSort";
 
 const App = () => {
+	const [isloggedin, setIsloggedin] = useState(false);
+	const [todos, setTodos] = useState([]);
 
-  const [isloggedin, setIsloggedin] = useState(false);
-  const [todos, setTodos] = useState([]);
+	useEffect(() => {
+		const fetchTodos = async () => {
+			try {
+				const res = await axios.get("/api/todos");
+				setTodos(res.data);
+			} catch (err) {
+				console.error("Error finding todos", err);
+			}
+		};
 
-  useEffect(() => {
+		fetchTodos();
+	}, []);
 
-    const fetchTodos = async () => {
-      
-      try {
-        const res = await axios.get('/api/todos');
-        setTodos(res.data);
+	const handleLogin = () => {
+		setIsloggedin((prev) => !prev);
+	};
 
-      } catch (err) {
-        console.err('Error finding todos', err);
-      }
-    }
+	const handleLoginFormSubmit = (username, password) => {
+		const login = async () => {
+			try {
+				const res = await axios.post("/api/login", {
+					username,
+					password,
+				});
+				console.log(res.data);
+				setIsloggedin(true);
+			} catch (err) {
+				console.error("Error logging in", err);
+			}
+		};
 
-    fetchTodos();
+		login();
+	};
 
-  }, []);
+	const handleEditTodo = (editedTodo) => {
+		const updateTodo = async () => {
+			try {
+				const res = await axios.put(
+					`/api/todos/${editedTodo.id}`,
+					editedTodo
+				);
+				const updatedTodos = todos.map((todo) =>
+					todo._id === editedTodo._id ? res.data : todo
+				);
+				setTodos(updatedTodos);
+			} catch (err) {
+				console.error("Error updating todo", err);
+			}
+		};
 
-  const handleLogin = () => { 
-    setIsloggedin( prev => !prev )
-  }
+		updateTodo();
+	};
 
-  const handleLoginFormSubmit = (username, password) => {
+	const handleSortTodos = () => {
+		const sortTodos = async () => {
+			try {
+				const res = await axios.get("/api/todos/sort");
+				setTodos(res.data);
+			} catch (err) {
+				console.error("Error sorting todos", err);
+			}
+		};
 
-    const login = async () => {
-      try {
-        const res = await axios.post('/api/login', { username, password });
-        console.log(res.data);
-        setIsloggedin(true);
+		sortTodos();
+	};
 
-      } catch (err) {
-        console.err('Error logging in', err);
-      }
-    }
+	const handleCreateTodo = (newTodo) => {
+		const createTodo = async () => {
+			try {
+				const res = await axios.post("/api/todos", newTodo);
+				setTodos([...todos, res.data]);
+			} catch (err) {
+				console.error("Error creating todo", err);
+			}
+		};
 
-    login();
-  }
+		createTodo();
+	};
 
-  return (
-    <div className="App">
-      
-      <NavBar
-        isloggedin={isloggedin}
-        handleLogin={handleLogin}
-      />
-      {!isloggedin && <LoginForm onLogin={handleLoginFormSubmit} />}
+	return (
+		<div className="App">
+			<NavBar isloggedin={isloggedin} handleLogin={handleLogin} />
+			{!isloggedin && <LoginForm onLogin={handleLoginFormSubmit} />}
 
-    </div>
-  );
-}
+			{isloggedin && (
+				<div>
+					<TodoCreate onCreate={handleCreateTodo} />
+					<TodoSort onSort={handleSortTodos} />
+					<TodoList todos={todos} onEdit={handleEditTodo} />
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default App;
