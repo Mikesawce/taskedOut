4670fba34ab6a06f9d5172ebb37aa4cd7974929d
Michael Mote <michael.mote1993@gmail.com>
2023-12-21 21:58:26 -0600

    login form created

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+
+const LoginForm = ({ onLogin }) => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        onLogin(username, password);
+    }
+
+    return (
+        <form onSubmit={handleLogin}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </label>
+            <button type="submit">Login</button>
+        </form>
+    )
+};
+
+export default LoginForm;
