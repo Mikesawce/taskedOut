90de4372ee9d18ee52e572b977fc615268e302ba
Michael Mote <michael.mote1993@gmail.com>
2023-12-21 23:28:09 -0600

    todoList

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, handleEditTodo }) => {
 
     return (
         <>
             <h2>Your tasks!</h2>
             <ul>
                 {todos.map(todo => (
-                    <li key={todo.id}>
+                    <li key={todo.id} onClick={handleEditTodo}>
                         <strong>{todo.title}</strong> - {todo.description} (priority: {todo.priority})
                     </li>
                 ))}
