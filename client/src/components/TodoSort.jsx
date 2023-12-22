d03e9c0f5881050d98c2b35e3cdc9b0b12de4905
Michael Mote <michael.mote1993@gmail.com>
2023-12-21 23:28:15 -0600

    Sort in progress

---
+++ client/src/components/TodoSort.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const TodoSort = ({ onSort }) => {
+
+    return (
+        <button onClick={onSort}>Sort by Priority</button>
+    );
+};
+
+export default TodoSort;
